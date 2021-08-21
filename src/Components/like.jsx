8d3726a949fa100977f497ad86e037ca92e7c645
Veena Krishna Sandana <43022790+Veenakrishna2313@users.noreply.github.com>
2021-08-20 19:14:01 -0700

    converted like from class  to functional component

--- src/Components/like.jsx
+++ src/Components/like.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from 'react'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-class like extends Component {
-  state = { 
-
-   }
-  render() { 
-console.log("like",this.props.movie.likes);
-    let classes="fa-heart";
-      classes +=this.props.movie.likes===false ? " far": " fas";
-      console.log("classes",classes);
-    return <i  style={{cursor:'pointer'}} onClick={this.props.onClicking} className={classes}></i>;
-     
-  }
+const Like = props => {
+  
+  console.log("like",props.movie.likes);
+      let classes="fa-heart";
+        classes +=props.movie.likes===false ? " far": " fas";
+        console.log("classes",classes);
+      return <i  style={{cursor:'pointer'}} onClick={props.onClicking} className={classes}></i>;
+    
 }
  
-export default like;
+export default Like;
