6d668a7323f1ae936ddcc5a7dae4432dc3d2a935
Veena Krishna Sandana <43022790+Veenakrishna2313@users.noreply.github.com>
2021-09-07 12:36:01 -0700

    login form complete

--- src/Components/common/listGroup.jsx
+++ src/Components/common/listGroup.jsx
@@ -5,12 +5,15 @@ const ListGroup = ({items, textProperty, valueProperty, ItemSelect, selectedItem
    console.log("Props for genre");
   return (      
         <ul className="list-group">
-        {items.map((item)=>
-        <li key={item[valueProperty]} onClick={()=>ItemSelect(item)} className={item===selectedItem?"list-group-item clickable active": "list-group-item clickable disabled"}>{item[textProperty]}</li>
-
-     )}
+        {
+            items.map((item)=>
+            <li key={item[valueProperty]} onClick={()=>ItemSelect(item)} className={item===selectedItem?"list-group-item active clickable": "list-group-item clickable"} >{item[textProperty]}</li>
+            )}
+            
+            {/* items.map((item)=>
+            <li key={item[valueProperty]} onClick={()=>console.log("I got clicked")} className={item===selectedItem?"list-group-item clickable active": "list-group-item clickable disabled"}>{item[textProperty]}</li>
+            )} */}
         </ul>
-   
     );
 
     }
--- src/Components/common/loginForm.jsx
+++ src/Components/common/loginForm.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
 
 class LoginForm extends Component {
+state={
+  account:{username:'', password:""}
+};
+
+  username=React.createRef();
+
+  // componentDidMount(){
+
+  //   this.username.current.focus();
+  // }
+
+  handleSubmit=e=>{
+    e.preventDefault();
+    console.log("form submitted");
+
+    const username=this.username.current.value;
+  }
+handleChange=(e)=>{
+
+  const account={...this.state.account};
+  account.username=e.currentTarget.value;
+  this.setState({account});
+}
+
   render() { 
     return <div> 
 
-    <form >
-      <div className="form-group"><label htmlFor="username">User Name</label><input id="username" type="text" className="form-control" /></div>
+    <form onSubmit={this.handleSubmit}>
+      <div className="form-group"><label htmlFor="username">User Name</label><input value={this.state.account.username} onChange={this.handleChange} autoFocus ref={this.username} id="username" type="text" className="form-control" /></div>
       <div className="form-group"><label htmlFor="password">Password</label><input id="password" type="text" className="form-control" /></div>
     <button className="btn btn-primary">Save</button>
     </form>
