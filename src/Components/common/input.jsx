243883baf38e03d0e023a05ec444045684124d4d
Veena Krishna Sandana <43022790+Veenakrishna2313@users.noreply.github.com>
2021-09-30 23:26:48 -0700

    login complete

--- src/Components/common/input.jsx
+++ src/Components/common/input.jsx
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-const Input = (({name, label, value, onChange}) => {
+const Input = (({name, label, error, ...rest}) => {
   return ( 
      <div className="form-group">
        <label htmlFor={name}>{label}</label>
-       <input value={value} onChange={onChange} id={name} name={name} type="text" className="form-control" /></div>
+       <input {...rest} name={name} id={name} className="form-control" />
+       {error && <div className="alert alert-danger">{error}</div>}
+       </div>
+
    );
 })
  
--- src/Components/common/loginForm.jsx
+++ src/Components/common/loginForm.jsx
@@ -1,42 +1,33 @@
-import React, { Component } from 'react';
-import { AccordionButton } from 'react-bootstrap';
-import Input from './input';
-
-class LoginForm extends Component {
+import React from 'react';
+import Joi from 'joi-browser';
+import Form from './form';
+class LoginForm extends Form {
 state={
-  account:{username:'', password:""}
+  data:{username:'', password:""},
+  errors:{
+
+  }
 };
 
-  username=React.createRef();
-
-  // componentDidMount(){
-
-  //   this.username.current.focus();
-  // }
-
-  handleSubmit=e=>{
-    e.preventDefault();
-    console.log("form submitted");
-
-    const username=this.username.current.value;
-  }
-handleChange=({currentTarget:input})=>{
-
-  const account={...this.state.account};
-  account[input.name]=input.value;
-  this.setState({account});
+schema={
+  username:Joi.string().required().label("Username"),
+  password:Joi.string().required().label("Password")
 }
+  doSubmit=()=>{
+    //call the server
+      console.log(" form submitted")
+  }
+
 
   render() { 
-
-    const {account}=this.state;
-    
+   
     return <div> 
     <h1 className="mb-3">Login </h1>
     <form onSubmit={this.handleSubmit}>
-      <Input  name="username" value={account.username} label="Username" onChange={this.handleChange} />
-      <Input  name="password" value={account.password} label="Password" onChange={this.handleChange} />
-    <button className="btn btn-primary mt-3">Save</button>
+     { this.renderInput("username", "Username")}
+     { this.renderInput("password", "Password", "password")} 
+      {this.renderButton("login")}
+      
     </form>
 
     </div>;
